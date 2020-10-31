f2769748c1d4917b588ffde038648f7f2927dba4
Erdianna <17IMC10370@fh-krems.ac.at>
2020-10-31 12:53:49 +0100

    setup

--- Werewolves/Werewolves/urls.py
+++ Werewolves/Werewolves/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index), # start has to be name of the function
 ]
